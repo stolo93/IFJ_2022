4849c128597a6128669e4b2bb884b2230252e0d2
Jozef Michal Bukas <xbukas00@stud.fit.vutbr.cz>
2022-10-08 22:31:07 +0200

    last small change

---
+++ src/headers/scaner.h
@@ -0,0 +1,30 @@
+/****************************************************************
+ * @name scanner.h
+ * @author : Jozef Michal Bukas <xbukas00@stud.fit.vutbr.cz>
+ * Subject : IFJ
+ * Project : Compiler for a given subset of the php language
+****************************************************************/
+#include <stdbool.h>
+#include "tokenStruct.h"
+
+#define DEFAULT_SIZE 20
+#define NUM_OF_KWORDS 7
+#define NUM_OF_DTYPES 3
+
+bool resizeString(char** string , unsigned int* size ) ;
+
+bool finalResize ( char** string) ;
+
+char * findDot( char* info ) ;
+
+tokenType checkForKeyword ( char* string ) ;
+
+token* convertNum ( token* newToken , char* info ) ;
+
+tokenType firstState( int character ) ;
+
+token* getToken () ;
+
+bool returnToken ( token* retToken ) ;
+
+bool checkProlog () ;
