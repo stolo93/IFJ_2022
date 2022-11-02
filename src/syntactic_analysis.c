8a4520d829e64065480617ed7d5edc0f9811d2ab
Samuel <xstola03@stud.fit.vutbr.cz>
2022-11-02 22:46:51 +0100

    Declarations of rules for non-terminals

--- src/syntactic_analysis.c
+++ src/syntactic_analysis.c
@@ -6,3 +6,86 @@
  * @date 2022-11-02
  *
  */
+
+#include "./headers/IFJ_2022.h"
+#include "./headers/syntactic_analysis.h"
+
+error( _Bool ) SA_Prolog ( token_t * Token)
+{
+
+}
+
+error( _Bool ) SA_Prog ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Body ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_ST_List ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Statement ( token_t * Token)
+{
+
+}
+
+error( _Bool ) SA_Fcal ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Fdef ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Rval ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_RetVal ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Args ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_ArgsNext ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Params ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_ParamsNext ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Type ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Term ( token_t * Token )
+{
+
+}
+
+error( _Bool ) SA_Expr ( token_t * Token )
+{
+
+}
